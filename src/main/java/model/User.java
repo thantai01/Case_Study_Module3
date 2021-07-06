7d8ad23f05280d276d1d9d45da10b604f6d7253e
thantai01 <thantaihp2011@gmail.com>
2021-07-06 10:15:21 +0700

    em Hung git lan dau

---
+++ src/main/java/model/User.java
@@ -0,0 +1,75 @@
+package model;
+
+public class User {
+    private String userID;
+    private String userPassword;
+    private String userAddress;
+    private String userFullName;
+    private String userPhone;
+    private int role;
+
+    public User(String userID, String userPassword,String userAddress ,String userFullName, String userPhone, int role) {
+        this.userID = userID;
+        this.userPassword = userPassword;
+        this.userAddress = userAddress;
+        this.userFullName = userFullName;
+        this.userPhone = userPhone;
+        this.role = role;
+    }
+
+    public User(String userID, String userPassword) {
+        this.userID = userID;
+        this.userPassword = userPassword;
+    }
+
+    public User() {
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+}
